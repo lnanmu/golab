579962c35cb79115988aaf4c6b775ba361d25f91
lnanmu@163.com <lnanmu@163.com>
2025-12-18 10:04:07 +0800

    fix: 删除切片中的元素

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,45 +4,17 @@ import "fmt"
 
 // 数组、切片、map、结构体、通道、指针、接口、函数
 
-//切片的常见应用:创建、增、删、改、查、复制
+
+var s []int = []int{1, 2, 3, 4, 5}
+var index int = 3
+func hi() {
+	s = append(s[:index],s[index+1:]...)
+
+}
 
 func main() {
-	//创建切片
-	s := make([]int, 0, 10)
-	for i := 10; i <= 100; i += 10 {
-		// 增加元素
-		s = append(s, i)
-	}
-	fmt.Println(s)
-	// 更改元素
-	for i, v := range s {
-		if v == 30 {
-			s[i] = 30000
-		}
-	}
-	fmt.Println(s)
+	fmt.Println("改变前的数组：", s)
+	hi()
+	fmt.Println("改变后的数组", s)
 
-	// 删除元素
-	y := cap(s)
-
-	s1 := make([]int, 0, y)
-
-	for _, v := range s {
-		if v == 30000 {
-			continue
-		}
-		s1 = append(s1, v)
-
-	}
-	s = s1
-	fmt.Println(s)
-	//查询
-	for k, v := range s {
-		fmt.Printf("k:%d, v:%d\n", k, v)
-	}
-
-	s2 := make([]int, 2, 2)
-	// copy的结果是有dts的长度决定的
-	copy(s2, s)
-	fmt.Println(s2, s)
 }
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,10 @@
 package db
+
+
+var s []int = []int{1, 2, 3, 4, 5}
+var index int = 3
+// 删除数组里指定位置的元素，注意append的用法，第二个位置上是元素，所以使用...拆解切片
+func hi() {
+	s = append(s[:index],s[index+1:]...)
+
+}
