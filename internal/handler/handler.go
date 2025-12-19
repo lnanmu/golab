266aad0d5d94804e5cc75bda8be21d2429085851
lnanmu@163.com <lnanmu@163.com>
2025-12-19 16:35:55 +0800

    fix: 一些简单的小练习

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,7 +1,63 @@
-package handler 
+package handler
 
 import "fmt"
 
 func Hello() {
 	fmt.Println("Hello, This is handler")
+}
+
+var s []int = []int{5, 12, 9, 27, 18, 3, 22}
+
+func sum(s []int) int {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	return sum
+}
+
+func max10(s []int) []int {
+	s1 := make([]int, len(s), cap(s))
+	for _, v := range s {
+		if v > 10 {
+			s1 = append(s1, v)
+		}
+	}
+	return s1
+}
+
+// 找到最小的
+func min(s []int) int {
+	minnumber := s[0]
+	for _, v := range s {
+		if v < minnumber {
+			minnumber = v
+		}
+	}
+	return minnumber
+}
+
+// 找到最大的
+func max(s []int) int {
+	maxnumber := s[0]
+	for _, v := range s {
+		if v > maxnumber {
+			maxnumber = v
+		}
+	}
+	return maxnumber
+}
+
+//找到最小的和最大的，这两个函数都有一种思想，就是初始化条件的思想，先假设第一个就是最小的或者最大的，然后遍历数组。如果存在更小的那么就更新最小的，如果没有更小的就不更新，就继续遍历，直到遍历结束。 每一次都是经过对比过的 每一次遍历都是在上一次的对比的结果。。
+
+func mainkk() {
+	he := sum(s)
+
+	maxn := max(s)
+
+	s2 := max10(s)
+
+	minn := min(s)
+
+	fmt.Println(he, maxn, s2, minn)
 }
--- internal/model/model.go
+++ internal/model/model.go
@@ -6,11 +6,9 @@ func ModelInfo() {
 	fmt.Println("This is Model")
 }
 
-
-
 type Student struct {
-	ID int
-	Name string 
+	ID    int
+	Name  string
 	Score []int // 是一个切片，存放成绩
 
 }
@@ -18,28 +16,29 @@ type Student struct {
 var s1 []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 // 返回学生的三科目的平均分数
-func scoreing(s Student) int{
+func scoreing(s Student) int {
 	sum := 0
-	for _, v := range s.Score{
+	for _, v := range s.Score {
 		sum += v
 	}
 	so := sum / len(s.Score)
 	return so
 }
+
 // 返回一个切片中所有的偶数，创建一个新的切片
 func oushu(s []int) []int {
-	s2 := make([]int,0, len(s))
+	s2 := make([]int, 0, len(s))
 	for _, v := range s {
-		if v % 2 ==0 {
+		if v%2 == 0 {
 			s2 = append(s2, v)
 		}
 	}
 	return s2
 }
 
-// 查找20以内所有的质数字 
+// 查找20以内所有的质数字
 func zhishu() []int {
-	s := make([]int,0, 20)
+	s := make([]int, 0, 20)
 	// 外层控制，是寻找质数的范围
 	for i := 2; i <= 20; i++ {
 		// 内层控制，判断i是否为质数
@@ -47,7 +46,7 @@ func zhishu() []int {
 		// 因为如果是i偶数，那么出了2以外，一定不是质数，那么就只剩下奇数的部分了，奇数部分只需要判断到i的一半就可以了，因为大于一半的数字不可能是因子，因为如果是因子，那么商就会是小于2，这样就不是整除了
 		// 如果出现了能整除的数字，那么就不是质数，结束内层循环就可以
 		for j := 2; j <= i/2; j++ {
-			if i % j ==0 {
+			if i%j == 0 {
 				flag = false
 				break
 			}
@@ -60,13 +59,14 @@ func zhishu() []int {
 
 	return s
 }
+
 // 我改如何理解flag这种操作，这里引入一个状态追的思想，因为整个程序的运行过程是动态的，我们需要根据不同的状态来做出不同的操作，所以要通过一个表达式来进行判断这种状态是否满足我们的要求，所以我们引入了一个标记位，我们通过判断标记位的的值来判断我们改进行如何操作，有了标记位之后，就可以有一个比较清晰的表达式，这样更简单，好好体会一下这个思想，标记位的变化是随着数据或者状态的变化而变化。
-func zhishu2 () []int  {
+func zhishu2() []int {
 	s := make([]int, 0, 20)
 	for i := 37; i <= 200; i++ {
 		flag := true
 		for j := 2; j <= i/2; j++ {
-			if i % j ==0 {
+			if i%j == 0 {
 				flag = false
 				break
 			}
@@ -79,15 +79,13 @@ func zhishu2 () []int  {
 }
 
 // ++的使用 记住了
-func zifununber (s string) map[rune]int {
-	m := make(map[rune] int)
+func zifununber(s string) map[rune]int {
+	m := make(map[rune]int)
 	for _, v := range s {
-		m[v] ++
+		m[v]++
 	}
 	return m
-} 
-
-
+}
 
 func mainjjj() {
 	s := zhishu()
@@ -97,4 +95,31 @@ func mainjjj() {
 	m := zifununber("abcabc")
 	fmt.Println(m)
 
+}
+
+func news(s []int) map[int]bool {
+	m := make(map[int]bool)
+	// s1 := []int{}
+	for _, v := range s {
+		m[v] = true
+	}
+	fmt.Println(m)
+
+	return m
+}
+
+// map中的不能重复是只map的key必须是唯一的！！！！ 还是要了解底层的远离，否则不知道怎么写
+
+func news1(m map[int]bool) []int {
+	s := []int{}
+	for k, _ := range m {
+		s = append(s, k)
+	}
+	return s
+}
+
+func mainlkhgj() {
+	s := []int{3, 5, 3, 7, 5, 9, 7, 10}
+	s1 := news(s)
+	fmt.Println(s1)
 }
